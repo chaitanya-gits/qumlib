5ddf82209caa4f94a5785f2be7bc3f060f1bd28b
chaitaanya10 <chaitanyavalluru999@gmail.com>
2026-02-11 16:59:24 +0530

    Initial commit

--- src/qumlib/gates.py
+++ src/qumlib/gates.py
@@ -12,9 +12,42 @@ Z = np.array([[1, 0],
 H = (1/np.sqrt(2)) * np.array([[1, 1],
                                [1, -1]], dtype=complex)
 
+I = np.array([[1, 0],
+              [0, 1]], dtype=complex)
+
+T = np.array([[1, 0],
+              [0, np.exp(1j * np.pi / 4)]], dtype=complex)
+
+S = np.array([[1, 0],
+              [0, 1j]], dtype=complex)
+
 CNOT = np.array([
     [1,0,0,0],
     [0,1,0,0],
     [0,0,0,1],
     [0,0,1,0]
+], dtype=complex)
+
+CZ = np.array([
+    [1,0,0,0],
+    [0,1,0,0],
+    [0,0,1,0],
+    [0,0,0,-1]
+], dtype=complex)
+
+SWAP = np.array([
+    [1,0,0,0],
+    [0,0,1,0],
+    [0,1,0,0],
+    [0,0,0,1]], dtype=complex)
+
+CXX = np.array([
+    [1,0,0,0,0,0,0,0],
+    [0,1,0,0,0,0,0,0],
+    [0,0,1,0,0,0,0,0],
+    [0,0,0,1,0,0,0,0],
+    [0,0,0,0,-1j,-1j,-1j,-1j],
+    [0,0,0,-1j,-1j,-1j,-1j,-1j],
+    [0,-1j,-1j,-1j,-1j,-1j,-1j,-1j],
+    [-1j,-1j,-1j,-1j,-1j,-1j,-1j,-1j]
 ], dtype=complex)
